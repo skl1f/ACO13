788191e609a442325bb8e030579d2d58bcd0e49f
skl1f <skl1f@ukrgadget.com>
2016-05-24 12:05:16 +0300

    home MyString

--- src/week1/day1/Test.java
+++ src/week1/day1/Test.java
@@ -7,8 +7,6 @@ public class Test {
     public static void main(String[] args) {
         Human person = new Human("Kolja", 25, "M", 180, 85);
 
-
-
         /*String personInfo = person.asString();
         System.out.println(personInfo);*/
 
