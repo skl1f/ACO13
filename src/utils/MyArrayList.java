7a10a69f7762087cf9b44d59b15d1ba336bb9a44
skl1f <skl1f@ukrgadget.com>
2016-06-01 12:43:42 +0300

    MyArrayList implemented

---
+++ src/utils/MyArrayList.java
@@ -0,0 +1,83 @@
+package utils;
+
+
+import java.util.Arrays;
+
+/**
+ * Created by skl1f on 6/1/2016.
+ */
+public class MyArrayList {
+    private final int DEFAULT_SIZE = 0;
+    private Object[] array;
+
+    public MyArrayList() {
+        array = new Object[DEFAULT_SIZE];
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MyArrayList{%s}", Arrays.toString(array));
+    }
+
+    public Object get(int index) {
+        return array[index];
+    }
+
+    public void add(Object obj) {
+        Object[] newArray = new Object[size() + 1];
+        System.arraycopy(array, 0, newArray, 0, size());
+        newArray[size()] = obj;
+        array = newArray;
+    }
+
+    public void add(int index, Object obj) {
+        Object[] newArray = new Object[size() + 1];
+        System.arraycopy(array, 0, newArray, 0, index);
+        newArray[index] = obj;
+        System.arraycopy(array, index, newArray, index + 1, size() - 1);
+        array = newArray;
+
+    }
+
+    public Object remove(int index) {
+        Object[] newArray = new Object[size() - 1];
+        System.arraycopy(array, 0, newArray, 0, index);
+        System.arraycopy(array, index + 1, newArray, index, size() - index - 1);
+        array = newArray;
+        return array[index];
+    }
+
+    public boolean remove(Object obj) {
+        for (int index = 0; index <= size(); index++) {
+            if (get(index).equals(obj)) {
+                remove(index);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean contains(Object obj) {
+        for (int index = 0; index < size(); index++) {
+            if (get(index).equals(obj)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Object set(int index, Object obj) {
+        Object result = array[index];
+        array[index] = obj;
+        return result;
+    }
+
+    public void clear() {
+        array = new Object[DEFAULT_SIZE];
+    }
+
+    public int size() {
+        return array.length;
+    }
+
+}
---
+++ src/week2/home/MyArrayListTest.java
@@ -0,0 +1,26 @@
+package week2.home;
+
+import utils.MyArrayList;
+
+/**
+ * Created by skl1f on 6/1/2016.
+ */
+public class MyArrayListTest {
+    public static void main(String[] args) {
+        MyArrayList arrayList = new MyArrayList();
+        arrayList.add("One");
+        arrayList.add("Second");
+        arrayList.add(1, "2");
+
+        System.out.println(arrayList);
+
+        arrayList.remove("2");
+        System.out.println("arrayList contains \"2\": " + arrayList.contains("2"));
+        arrayList.set(0, "1");
+        System.out.println("arrayList contains \"1\": " + arrayList.contains("1"));
+        System.out.println(arrayList);
+        arrayList.clear();
+        System.out.println(arrayList);
+    }
+
+}
